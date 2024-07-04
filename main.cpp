142c7fc9ff5d33b1744af795b8884402095ce1ad
IaroslavElistratov <iaroslav.elistratov@gmail.com>
2024-07-04 18:13:58 +0300

    rand num generator

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
-#include <iostream>
+#include <iostream> // todo: use C only
+// #include <time.h>
+// #include <stdlib.h>
+
+
 using namespace std;
+
 
 // note: assumes a, b are same shape
 int* ElementwiseMul(int* a, int* b, int size)
@@ -29,17 +34,43 @@ void Print(int* a, int size)
     cout << endl;
 }
 
+
+float* GenRandomFloat()
+{
+    float* f_prt = (float*)malloc(sizeof(float));
+
+    // https://linux.die.net/man/3/random
+    // returns a pseudo-random int between 0 and RAND_MAX
+    // normalize to: 0 - 1
+    // shift to: -0.5 - 0.5
+    float r = ((float)rand() / RAND_MAX) - 0.5;
+
+    // write to heap memory
+    *f_prt = r;
+
+    return f_prt;
+}
+
+
 int main() {
-    int x[5] = {2, 4, 5, 8, 1};
-    int w[5] = {2, 2, 2, 2, 2};
+    // random num generator init, must be called once
+    srand(time(NULL));
+    // srand(123);
 
-    int size = sizeof(x) / sizeof(int);
-    // cout << size;
+    float* f = GenRandomFloat();
+    cout << "GenRandomFloat: " << *f << endl;;
 
-    // int* out = ElementwiseMul(x, w, size);
-    // Print(out, size);
+    // // todo: make it random numbers
+    // int x[5] = {2, 4, 5, 8, 1};
+    // int w[5] = {2, 2, 2, 2, 2};
 
-    int* out = Matmul1d(x, w, size);
-    cout << *out << endl;
+    // int size = sizeof(x) / sizeof(int);
+    // // cout << size;
+
+    // // int* out = ElementwiseMul(x, w, size);
+    // // Print(out, size);
+
+    // int* out = Matmul1d(x, w, size);
+    // cout << *out << endl;
     return 0;
 }
